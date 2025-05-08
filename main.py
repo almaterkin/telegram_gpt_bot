ba84c9ffac452d329a20749ef91c0144165f3da7
almaterkin <almatsherman@gmail.com>
2025-05-08 16:47:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -165,4 +165,7 @@ async def main():
     )
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import nest_asyncio
+    nest_asyncio.apply()
+    asyncio.get_event_loop().run_until_complete(main())
+
